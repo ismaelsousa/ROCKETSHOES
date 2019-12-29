3c50a2a79ee98fe2d106d9dcdc6c7618243438c0
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2019-12-29 13:36:45 -0300

    Organizando git

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { MdShoppingBasket } from 'react-icons/md';
+import { Container, Cart } from './styles';
+import Logo from '../../assets/images/logo.svg';
+
+/** cartSize é o nome que eu quiz dar para a props */
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length)
+  return (
+    <Container>
+      <Link to="/">
+        <img src={Logo} alt="RocketShoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>{cartSize} itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+}
+
+
